7af665af2ca71629e753d02461569533e90a6ce1
Jayson <Jayson2050@163.com>
2022-01-22 11:12:34 +0800

    添加debug调试打印封装示例带啊吗

--- debug_print/debug_print.c
+++ debug_print/debug_print.c
@@ -14,8 +14,12 @@
 void main() 
 {
 	int flag = 1;
-    unsigned int a = 1, b=2; 
-    DBG_PRINT(flag, "test 0x%02x 0x%02x\r\n", a,b);
+    unsigned int a = 1, b=2;
+	unsigned char array[8] = {0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x88};
+
+    //DBG_PRINT(flag, "test 0x%02x 0x%02x\r\n", a,b);
+	printf("array : %x \r\n", *array);
+	
 	return;
 }
 
